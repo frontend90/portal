0ccb133c4570169add9f173b338932aa3e1f667b
Vasiliy Pupkin <vpupkin@company.com>
2020-05-02 19:32:06 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
+/*********************  плавный скролл    ***************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
+
 /************    burger      *************/
 $('.burger').click(function () {
   $('.burger').toggleClass('active');
   $('.menu-link').toggleClass('show');
+  $('body').toggleClass('no-scroll');
 });
 
 $('.menu-link a').click(function () {
   $('.menu-link').removeClass('show');
   $('.burger').removeClass('active');
+  $('body').removeClass('no-scroll');
+});
+
+/************    header slick      **************/
+$('.header-slider').slick({
+  prevArrow: $('.header-arrow-left'),
+  nextArrow: $('.header-arrow-right'),
+  dots: false,
+  responsive: [{
+    breakpoint: 1066,
+    settings: {
+      arrows: false
+    }
+  }]
 });
